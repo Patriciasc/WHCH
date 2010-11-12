f2a2dd956a2244440a7dfd801f1abb7e59b8b276
Patricia Santana Cruz <patriciasc@openismus.com>
2010-11-12 12:32:14 +0100

    Added license introduction in every source file.

--- whch.h
+++ whch.h
@@ -4,6 +4,34 @@
 #include <QMainWindow>
 #include "whchTableModel.h"
 #include <QFile>
+/*
+ * ########################################################################
+ * # File: WHCH (Working Hours Counter for Humans)- whch.h                #
+ * #                                                                      #
+ * # Author: Patricia Santana Cruz  <patriciasc@openismus.com>            #
+ * #                                                                      #
+ * # Copyright (C) 2010 Openismus GmbH                                    #
+ * #                                                                      #
+ * # Version: 0.1                                                         #
+ * #                                                                      #
+ * # Description: Application for tracking the time spent on individual   #
+ * # projects.                                                            #
+ * #                                                                      #
+ * # WHCH is free software; you can redistribute it and/or modify         #
+ * # it under the terms of the GNU General Public License as published by #
+ * # the Free Software Foundation; either version 3 of the License,       #
+ * # or (at your option) any later version.                               #
+ * #                                                                      #
+ * # WHCH is distributed in the hope that it will be useful,              #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    #
+ * # General Public License for more details.                             #
+ * #                                                                      #
+ * # You should have received a copy of the GNU General Public License    #
+ * # along with WHCH. If not, see <http://www.gnu.org/licenses/>.         #
+ * ########################################################################
+*/
+
 #include <QDomElement>
 #include <QDomDocument>
 #include <QDialog>
